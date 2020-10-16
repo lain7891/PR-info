0716071f91e8834872fcfb87a631d119e970fb2f
Estefania <nialupo87@gmail.com>
2020-10-16 14:27:15 -0400

    added background

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 console.log("Hello World");
 $(document).ready(function () {
   console.log("ready");
+
+  $(document).ready(function(){
+    $(".background").css("background-image", "url('City.jpg')");
+ });
+
   var queryURL =
     "https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=be3ea934&app_key=92b7a058356afbbaa6b1cf90c7bae1c1&results_per_page=20&what=javascript%20developer&content-type=application/json";
   $.ajax({
