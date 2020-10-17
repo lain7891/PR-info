e16f2d76335bbb510cfe05aba5a3c06a691e2c7e
Estefania <nialupo87@gmail.com>
2020-10-16 22:26:40 -0400

    trying to create various images in the home page

--- script.js
+++ script.js
@@ -2,9 +2,27 @@ console.log("Hello World");
 $(document).ready(function () {
   console.log("ready");
 
-  $(document).ready(function(){
-    $(".background").css("background-image", "url('City.jpg')");
- });
+  
+  //   $(".background").css("background-image", "url('images/City.jpg')");
+  // });
+
+
+  var images = ["images/City.jpg", "images/mountain.jpg", "images/small-city.jpeg", "images/malltowns.jpg"];
+
+  $(function () {
+    var i = 0;
+    $("#dvImage").css("background-image", "url(images/" + images[i] + ")");
+    setInterval(function () {
+        i++;
+        if (i == images.length) {
+            i = 0;
+        }
+        $("#dvImage").fadeOut("slow", function () {
+            $(this).css("background-image", "url(images/" + images[i] + ")");
+            $(this).fadeIn("slow");
+        });
+    }, 1000);
+});
 
   var queryURL =
     "https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=be3ea934&app_key=92b7a058356afbbaa6b1cf90c7bae1c1&results_per_page=20&what=javascript%20developer&content-type=application/json";
@@ -15,3 +33,4 @@ $(document).ready(function () {
     console.log(response);
   });
 });
+
